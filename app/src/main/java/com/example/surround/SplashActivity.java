138b32914649fdbe6bd64d88710e6ad8118bc034
AlexGz9851 <a00820225@itesm.mx>
2020-03-10 19:51:23 -0600

    firstCommit

---
+++ app/src/main/java/com/example/surround/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.example.surround;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+        private long ms=0;
+        private long splashTime=5000;
+        private boolean splashActive = true;
+        private boolean paused=false;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splash);
+            animation();
+            Thread mythread = new Thread() {
+                public void run() {
+                    try {
+                        while (splashActive && ms < splashTime) {
+                            if(!paused)
+                                ms=ms+100;
+                            sleep(100);
+                        }
+                    } catch(Exception e) {}
+                    finally {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            };
+            mythread.start();
+        }
+
+        private void animation(){
+            ConstraintLayout cl = findViewById(R.id.bkg_splash);
+            AnimationDrawable ad = (AnimationDrawable) cl.getBackground();
+            ad.setEnterFadeDuration(400);
+            ad.setExitFadeDuration(200);
+            ad.mutate();
+            ad.start();
+        }
+    }
