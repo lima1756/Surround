61c90b475fbfb0d74b41604c40773bca500102d7
Ivan Morett <luisivanmorett@gmail.com>
2020-03-28 13:48:11 -0600

    comentarios

--- src/constants/ControllerSignals.enum.ts
+++ src/constants/ControllerSignals.enum.ts
@@ -1,11 +1,13 @@
 enum ControllerSignals {
+    // envio
     LOGIN_RESPONSE = 'login_response',
+    CONFIGURE_SPEAKER_RESPONSE = 'configure_speaker_response',
+    PLAY_MUSIC_RESPONSE = 'play_response',
+    STOP_MUSIC_RESPONSE = 'stop_response',
+    // recibo    
     LOGIN = 'login_controller',
     CONFIGURE_SPEAKER='configure_speaker',
     PLAY_MUSIC='play',
     STOP_MUSIC='stop',
-    CONFIGURE_SPEAKER_RESPONSE = 'configure_speaker_response',
-    PLAY_MUSIC_RESPONSE = 'play_response',
-    STOP_MUSIC_RESPONSE = 'stop_response'
 }
 export default ControllerSignals;
--- src/constants/SpeakerSignals.enum.ts
+++ src/constants/SpeakerSignals.enum.ts
@@ -1,10 +1,12 @@
 enum SpeakerSignals {
+    // envio
     LOGIN_RESPONSE = 'login_response',
-    LOGIN = 'login_speaker',
-    READY = 'speaker_ready',
     PLAY = 'play',
     SET_MUSIC = 'set_music',
     STOP_SONG = 'stop_song',
     SET_TYPE = 'set_speaker',
+    // recibo
+    LOGIN = 'login_speaker',
+    READY = 'speaker_ready',
 }
 export default SpeakerSignals;
